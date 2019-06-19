d4c2859604b75b7532a744a5565dac854e00e4b5
Michał Kuwik <Michal7074@poczta.fm>
2019-06-19 16:55:32 +0200

    add smaller text for mobile under 678px width,add hover for .nav-link item, add animation onClick on icon TV element

--- src/js/code.js
+++ src/js/code.js
@@ -1,13 +1,20 @@
+//get DOM Elements
 const burgerMenu = document.querySelector('.hamburger-menu');
 const burgerItems = document.querySelectorAll('.line');
 const naviMenu = document.querySelector('.nav-list');
+const navItems = document.querySelectorAll('.nav-link');
+const icon = document.querySelector('.fas');
 
 burgerMenu.addEventListener('click',function(){
   burgerItems.forEach(function(item){
     item.classList.toggle('active');
   });
 
-  naviMenu.classList.toggle('show');  
-  
+  naviMenu.classList.toggle('show');
+  navItems.forEach(function(ite){
+    ite.classList.toggle('animOp');
+  }) ;
+ 
+  icon.classList.toggle('iconAnime');
 });
 
