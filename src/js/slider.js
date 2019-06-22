076f5ace641065ab1db9b34a7809ddb9d53daf68
Michał Kuwik <Michal7074@poczta.fm>
2019-06-22 13:02:41 +0200

    create new file slider.js and slide 3 img

---
+++ src/js/slider.js
@@ -0,0 +1,28 @@
+
+const slideList = ['img/tv-left.png','img/tv-left-2.png','img/tv-left-3.png']
+
+//Get Dom el
+const image = document.querySelector('img.slider');
+const dots = [...document.querySelectorAll('.dots .dot')];
+
+const dot1 = document.querySelector('.dot1');
+const dot2 = document.querySelector('.dot2');
+const dot3 = document.querySelector('.dot3')
+
+let i = 0;
+
+function changeImg(){
+  image.setAttribute('src', slideList[i]);
+  
+
+  if(i < slideList.length - 1){
+    i++;
+  }else{
+    i = 0;
+  }
+  setTimeout(changeImg, 5000);
+
+}
+
+window.onload = changeImg;
+
